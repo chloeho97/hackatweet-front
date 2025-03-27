0a3128f2356f81519d73a3d80c24f3303f72dec0
chloeho97 <chloe-ho@hotmail.fr>
2025-03-27 17:45:27 +0100

    Home

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,9 @@
 import styles from '../styles/Home.module.css';
 import Tweet from './Tweet'
 import LastTweets from './LastTweets'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Login from './Login'
+import { logout } from '../reducers/users';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
@@ -13,9 +14,15 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
 
-/*   const dispatch = useDispatch(); */
+const dispatch = useDispatch();
+
+const handleLogout = () => {
+  dispatch(logout())
+}
 
   const isConnected = useSelector((state) => state.users.value.token);
+  const username =  useSelector((state) => state.users.value.username);
+  const firstname =  useSelector((state) => state.users.value.firstname);
 
   const tweets = useSelector((state) => state.tweets.value)
 
@@ -26,52 +33,65 @@ function Home() {
   return (
     <div>
 
-      {isConnected ? ( <main className={styles.main}>
+      {!isConnected ? ( <Login /> ) : ( <main className={styles.main}>
 
-{/* HOME - LEFT */}
+        {/* HOME - LEFT */}
 
-<div className={styles.homeLeft}>
-  
-  <div>
-  <img className={styles.logo} src='./twitter-logo.png' />
-  </div>
-  <div className={styles.userInfo} >
-    <p>userInfo</p>
-  </div>
-</div>
+        <div className={styles.homeLeft}>
+          
+          <div>
+          <img className={styles.logo} src='./twitter-logo.png' />
+          </div>
 
+          <div className={styles.userSection}>
 
-{/* HOME - CENTER */}
-<div className={styles.homeCenter}>
-
-  <h2 className={styles.title}>
-    Home
-  </h2>
-
-  <div>
-    <Tweet />
-  </div>
-
-  <div>
-{/* {lastTweetsComponents} */}
-<p>CENTER PART 2</p>
-
-  </div>
-
-</div>
+              <div className={styles.userContent}>
+                <div className={styles.profilIcon}>
+                  <img className={styles.profilPictureIcon} src="./profilPicture.jpg"/>
+                </div>
+                <div className={styles.userInfo}>
+                    <div className={styles.userFirstname}>{firstname}</div>
+                    <div className={styles.userUsername}>@ {username}</div>
+              </div>
+          </div>
+            <button className={styles.btnLogout} id="logout" onClick={() => handleLogout()}>Logout</button>
+          </div>
 
 
-{/* HOME - RIGHT */}
-  <div className={styles.homeRight}>
-    <h2 className={styles.title}>Trends</h2>
-    <div>
+        </div>
 
-    </div>
-  </div>
-</main>
-) : ( <Login /> )}
-      </div>
-  );
-}
+
+        {/* HOME - CENTER */}
+        <div className={styles.homeCenter}>
+
+          <h2 className={styles.title}>
+            Home
+          </h2>
+
+          <div>
+            <Tweet />
+          </div>
+
+          <div>
+        {/* {lastTweetsComponents} */}
+        <p>CENTER PART 2</p>
+
+          </div>
+
+        </div>
+
+
+        {/* HOME - RIGHT */}
+          <div className={styles.homeRight}>
+            <h2 className={styles.title}>Trends</h2>
+            <div>
+
+            </div>
+          </div>
+        </main>
+        ) }
+              </div>
+          );
+        }
 
 export default Home;
